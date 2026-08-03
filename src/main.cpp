4208ba9616d4abaab0d826af9a87e93fd75ba277
Балашов Владислав <balashov.vladislav@csoft.ru>
2026-08-04 01:41:06 +0300

    Add class ShaderProgram

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
+#include "Renderer/ShaderProgram.h"
 
 GLfloat point[] = {
      0.0f,  0.5f, 0.0f,
@@ -93,26 +93,19 @@ int main(void)
 
 	glClearColor(1,1,0,1);
 
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, nullptr);
-    glCompileShader(vs);
-
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, nullptr);
-    glCompileShader(fs);
-    GLuint shader_program = glCreateProgram();
-    glAttachShader(shader_program, vs);
-    glAttachShader(shader_program, fs);
-    glLinkProgram(shader_program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    std::string vertex_shaider (vertex_shader);
+    std::string fragment_shader(fragment_shader);
+    Renderer::ShaderProgram shaderProgram (vertex_shader, fragment_shader);
+    if (!shaderProgram.isCompliled())
+    {
+        std::cerr << "Can't create shader program!"<<std::endl;
+        return -1;
+    }
 
     GLuint points_vbo = 0;
     glGenBuffers(1, &points_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
-
 
     GLuint colors_vbo = 0;
     glGenBuffers(1, &colors_vbo);
@@ -138,7 +131,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
+        shaderProgram.use();
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES,0,3);
 
