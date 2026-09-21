c1587c34fcc707744928eaa43bff90258635ae74
v.balashov <v.balashov@stc-spb.ru>
2026-09-21 18:56:29 +0300

    Промежуточный коммит

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,1 +1,12 @@
- 
+#include "ResourceManager.h"
+#include "../Renderer/ShaderProgram.h"
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+ResourceManager::ResourceManager(const std::string& executablePath)
+{
+    size_t found = executablePath.find_last_of("/\\");
+    m_path = executablePath.substr(0,found);
+}
